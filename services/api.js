7b55ebe646e816dac457a740adfa19122084b1e1
zerodois <felipe.rita@dcomp.sor.ufscar.br>
2019-08-05 17:30:52 -0300

    add .env

--- services/api.js
+++ services/api.js
@@ -1,6 +1,5 @@
 import 'isomorphic-fetch';
-import { formatUrl } from '../utils';
-const API_URL = 'http://localhost:8080';
+const { API_URL } = process.env;
 const request = {};
 
 const getRequestData = (method, data) => {
