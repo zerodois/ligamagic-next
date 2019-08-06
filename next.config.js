a613c52ff3da544ace22671e787aec5e03af06b0
zerodois <felipe.rita@dcomp.sor.ufscar.br>
2019-08-06 18:01:43 -0300

    add env to prefetch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-require('dotenv').config();
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
-  target: 'serverless'
+  target: 'serverless',
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+    return config
+  }
 })
